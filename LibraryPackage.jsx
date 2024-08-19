df4b8f5f78937829e5661bbbef2c4d9ff927b290
bharathijana1 <160519295+bharathijana1@users.noreply.github.com>
2024-08-19 20:56:26 +0530

    Add files via upload

---
+++ LibraryPackage.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+
+export const LibraryPackage = () => {
+  return (
+    <>
+    {/*
+    
+    npm create vite@latest
+    proj name:
+    react
+    javascript
+    cd proj-name
+    npm install
+    npm run dev
+
+    bootstrap
+    --------
+    npm install react-bootstrap
+    import 'bootstrap/dist/css/bootstrap.min.css';
+
+    Router
+    ------
+    npm install react-router-dom => import { BrowserRouter, Routes, Route } from "react-router-dom" & import { Link } from "react-router-dom"
+        <BrowserRouter>
+        <Navbar />
+        <Routes>
+        <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+        </BrowserRouter>
+
+        <Nav.Link href="#home">
+            <Link to="Home" className="nav-item text-decoration-none"><a href="" className="nav-link active text-white">Home</a></Link>
+            </Nav.Link>
+
+
+    Axios
+    -----
+
+    npm i axios => import axios from 'axios';
+
+    react use cart
+    --------------
+    npm i react-use-cart => import { useCart } from "react-use-cart" & import { CartProvider } from "react-use-cart"
+
+    Formik
+    ------
+
+    npm i formik yup
+
+    react scroll
+    ------------
+    
+    npm install react-scroll
+
+    top-loading-bar
+    ---------------
+
+    npm i react-top-loading-bar => import LoadingBar from 'react-top-loading-bar';
+
+    semantic-ui-react
+    -----------------
+
+    npm i semantic-ui-react => import { Button, Checkbox, Form } from 'semantic-ui-react';
+
+
+    // npm i @hookform/resolvers - for sing Schema => import {yupResolver } from '@hookform/resolvers/yup'
+    // npm i yup - for yup => import yup from 'yup'
+    // npm i react-hook-form - npm install => import { useForm } from 'react-hook-form'
+
+    react-icons
+    -----------
+
+    npm i react-icons => from react icons github website link for icons
+    EG:
+        import { FaGithub } from "react-icons/fa"; => <FaGithub />
+        import { SiNetlify } from "react-icons/si"; => <FaSquareWhatsapp />
+        import { IoMdMail } from "react-icons/io"; => <IoMdMail />
+        import { FaSquareWhatsapp } from "react-icons/fa6"; => <SiNetlify />
+    
+    Duplicate API
+    -------------
+
+    Jsonplaceholder - website
+
+
+
+
+
+
+
+
+
+
+    
+     */}
+    </>
+  )
+}
