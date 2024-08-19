0d022edccada2e1ddbcc0fa96ad0ee3e224f2816
bharathijana1 <160519295+bharathijana1@users.noreply.github.com>
2024-08-19 14:28:37 +0530

    Update HeaderNavbar.jsx

--- HeaderNavbar.jsx
+++ HeaderNavbar.jsx
@@ -13,31 +13,7 @@ export const HeaderNavbar = () => {
 
   return (
     <>
-
-    {/* <div className="container-fluied" style={{width:"100%"}}>
-
-    <header>
-        <nav expand="lg" className='navbar navbar-dark bg-dark navbar-expand-lg'>
-            <div className="container">
-                <a href="" className='navbar-brand'>ShoppingMart</a>
-                <button type='button' className='navbar-toggler' data-bs-toggle = "collapse" data-bs-target="#navbarnav">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id='navbarnav'>
-                    <ul className=' navbar-nav ms-auto'>
-                    <Link to="/" className="nav-item text-decoration-none"><a href="" className="nav-link active">Home</a></Link>
-                    <Link to="/card"  className="nav-item text-decoration-none"><a href=""className="nav-link">Cart</a></Link>
-
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
-    </div>
-     */}
-
-    {/* responsive navbar */}
-
+        
     <Navbar expand="lg" className="bg-dark">
       <Container>
         <Navbar.Brand href="#home" className='text-white'>ShoppingMart</Navbar.Brand>
